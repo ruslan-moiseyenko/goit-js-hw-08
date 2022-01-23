d8f5f4b1b6d89c8d92e5a33fa941570fcd228f29
Ruslan Moiseyenko <ruscom5@gmail.com>
2022-01-23 19:47:52 +0200

    gallery finished

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+
+const galleryWrap = document.querySelector(".gallery");
+
+galleryItems.forEach(({ preview, original, description }) => {
+  galleryWrap.insertAdjacentHTML('beforeend', `
+        <div>
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </div>
+        `);
+});
+
+
+const lightbox = new SimpleLightbox('.gallery a');
